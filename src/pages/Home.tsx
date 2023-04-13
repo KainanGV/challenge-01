d968716a94afbb67115a9253e80e682ee2918f2d
KainanGV <kainangabriel2019@gmail.com>
2023-04-13 12:43:59 -0300

    feat: finished part 1

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,32 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const task: Task = {
+      done: false,
+      id: new Date().getTime(),
+      title: newTaskTitle
+    }
+
+    setTasks((prev) => [...prev, task])
   }
 
-  function handleToggleTaskDone(id: number) {
+  function handleToggleTaskDone(id: number) {    
     //TODO - toggle task done if exists
-  }
+    const tasksNew = tasks.map(task => {
+      if(task.id === id) {
+        task.done = !task.done
+      }      
+
+      return task
+    });
+
+    setTasks(tasksNew)
+  }  
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const elementsFiltered = tasks.filter(task => task.id !== id)
+
+    setTasks(elementsFiltered)
   }
 
   return (
